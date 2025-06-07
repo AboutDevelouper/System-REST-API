ef3065f1cf21e17db224bbe4cb7750f7c422cb0e
About Develouper <127244634+AboutDevelouper@users.noreply.github.com>
2025-06-07 15:18:11 -0300

    Update and rename pageHome.js to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 const path = require("path")
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'docs', 'static', 'home.html'));
+  res.sendFile(path.join(__dirname, '../', 'public', 'home.html'));
 });
 
 router.get("/home", (req, res) => {
